25b3d54c71bc0699cbf84f4d61f773c5a0898ce8
olegsys <admin@olegsys.ru>
2021-12-01 14:34:56 +0500

    Fix key permission

--- key.tf
+++ key.tf
@@ -7,6 +7,6 @@ resource "aws_key_pair" "epam_key" {
   public_key = tls_private_key.epam_key.public_key_openssh
 
   provisioner "local-exec" { 
-    command = "echo '${tls_private_key.epam_key.private_key_pem}' > ./ansible/epam_key.pem"
+    command = "echo '${tls_private_key.epam_key.private_key_pem}' > ./ansible/epam_key.pem && chmod 600 ./ansible/epam_key.pem"    
   }
 }
